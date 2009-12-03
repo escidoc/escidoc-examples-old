4cb2c0c7f8aa2086d47540018a96c71fe96d85f3
Frank Schwichtenberg <Frank.Schwichtenberg@fiz-karlsruhe.de>
2009-12-03 13:47:52 +0000

    removed commandline scripts

--- CreateContainer.bat
+++
@@ -1,1 +0,0 @@
-"%JAVA_HOME%\bin\java" -cp eSciDocExamples.jar org.escidoc.simpleConnections.CreateContainer  %1 %2 %3
--- CreateItem.sh
+++
@@ -1,2 +0,0 @@
-$JAVA_HOME%/bin/java -cp eSciDocExamples.jar org.escidoc.simpleConnections.CreateItem  $1 $2 $3
-
