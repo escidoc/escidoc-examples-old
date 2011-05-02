6d38c54bb8d31637f3b448283946d2c48624895a
Joerg Hermann <Joerg.Hermann@fiz-karlsruhe.de>
2011-05-02 12:33:00 +0000

    transferred to infrastructure version 1.3; handler-client authentication adapted

--- src/java/org/escidoc/workingWithClientLib/RESTHandler/container/UpdateContainer.java
+++ src/java/org/escidoc/workingWithClientLib/RESTHandler/container/UpdateContainer.java
@@ -1,10 +1,13 @@
 package org.escidoc.workingWithClientLib.RESTHandler.container;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.escidoc.Constants;
 import org.escidoc.simpleConnections.Util;
 
+import de.escidoc.core.client.Authentication;
 import de.escidoc.core.client.exceptions.EscidocClientException;
 import de.escidoc.core.client.rest.RestContainerHandlerClient;
 
@@ -46,22 +49,26 @@ public class UpdateContainer {
     }
 
     private static String updateContainer(String containerId, String containerXml)
-        throws EscidocClientException {
-
-        RestContainerHandlerClient client = new RestContainerHandlerClient();
-        client.login(Util.getInfrastructureURL(), Constants.USER_NAME,
-            Constants.USER_PASSWORD);
+        throws EscidocClientException, MalformedURLException {
+    	
+    	// prepare client object
+        Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME, Constants.USER_PASSWORD);
+        RestContainerHandlerClient client = new RestContainerHandlerClient(auth.getServiceAddress());
+        client.setHandle(auth.getHandle());
+        
         String createdContainer = client.update(containerId, containerXml);
 
         return createdContainer;
     }
 
     private static String retrieveContainer(String containerId)
-        throws EscidocClientException {
-
-        RestContainerHandlerClient client = new RestContainerHandlerClient();
-        client.login(Util.getInfrastructureURL(), Constants.USER_NAME,
-            Constants.USER_PASSWORD);
+        throws EscidocClientException, MalformedURLException {
+    	
+    	// prepare client object
+        Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME, Constants.USER_PASSWORD);
+        RestContainerHandlerClient client = new RestContainerHandlerClient(auth.getServiceAddress());
+        client.setHandle(auth.getHandle());
+        
         String itemXml = client.retrieve(containerId);
 
         return itemXml;
