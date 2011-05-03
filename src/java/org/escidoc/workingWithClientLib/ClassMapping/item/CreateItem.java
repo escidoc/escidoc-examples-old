d451d98fdc501174782ba93e1a658020beca7b69
Joerg Hermann <Joerg.Hermann@fiz-karlsruhe.de>
2011-05-03 11:09:32 +0000

    use depositor name/password for item create

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/item/CreateItem.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/item/CreateItem.java
@@ -67,7 +67,7 @@ public class CreateItem {
 			ParserConfigurationException, MalformedURLException {
 		
 		// prepare client object
-		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN, Constants.USER_PASSWORD_SYSADMIN);
+		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_DEPOSITOR, Constants.USER_PASSWORD_DEPOSITOR);
 		ItemHandlerClient ihc = new ItemHandlerClient(auth.getServiceAddress());
 		ihc.setHandle(auth.getHandle());
 
