c6b99ccfe368441c0aaad0fd191884590bd171b7
Joerg Hermann <Joerg.Hermann@fiz-karlsruhe.de>
2011-05-03 10:57:54 +0000

    separate user name / password for sysadmin, inspector and depositor

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/container/CreateContainer.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/container/CreateContainer.java
@@ -67,7 +67,7 @@ public class CreateContainer {
 			ParserConfigurationException, MalformedURLException {
 		
 		// prepare client object
-		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME, Constants.USER_PASSWORD);
+		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN, Constants.USER_PASSWORD_SYSADMIN);
 		ContainerHandlerClient chc = new ContainerHandlerClient(auth.getServiceAddress());
 		chc.setHandle(auth.getHandle());
 
--- src/java/org/escidoc/workingWithClientLib/ClassMapping/container/ReleaseContainer.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/container/ReleaseContainer.java
@@ -71,7 +71,7 @@ public class ReleaseContainer {
 			throws EscidocClientException, MalformedURLException {
 
 		// prepare client object
-		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME, Constants.USER_PASSWORD);
+		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN, Constants.USER_PASSWORD_SYSADMIN);
 		ContainerHandlerClient chc = new ContainerHandlerClient(auth.getServiceAddress());
 		chc.setHandle(auth.getHandle());
 
