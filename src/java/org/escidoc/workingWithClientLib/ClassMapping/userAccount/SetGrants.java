04a21a7a7b4c3227a193ea8045a471d8d6ff3d5a
Joerg Hermann <Joerg.Hermann@fiz-karlsruhe.de>
2011-05-04 13:43:40 +0000

    example for adding a grant to an user account; draft

---
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/userAccount/SetGrants.java
@@ -0,0 +1,92 @@
+package org.escidoc.workingWithClientLib.ClassMapping.userAccount;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.escidoc.Constants;
+
+import de.escidoc.core.client.Authentication;
+import de.escidoc.core.client.UserAccountHandlerClient;
+import de.escidoc.core.client.exceptions.EscidocException;
+import de.escidoc.core.client.exceptions.InternalClientException;
+import de.escidoc.core.client.exceptions.TransportException;
+import de.escidoc.core.resources.aa.useraccount.Grant;
+import de.escidoc.core.resources.aa.useraccount.GrantProperties;
+import de.escidoc.core.resources.common.reference.Reference;
+import de.escidoc.core.resources.common.reference.RoleRef;
+
+/**
+ * Example how to add a grant to an existing user account.
+ * 
+ * @author JHE
+ * 
+ */
+public class SetGrants {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        try {
+
+            String userAccountId = "escidoc:user-account";
+            String contextId = "escidoc:context-id";
+            String roleObjectId = "escidoc:role-object-id";
+
+            if (args.length == 1) {
+                userAccountId = args[0];
+            }
+
+            if (args.length == 2) {
+                contextId = args[1];
+            }
+            
+            if (args.length == 3) {
+                roleObjectId = args[2];
+            }
+
+
+            Grant grant = new Grant();
+            GrantProperties grantProperties = new GrantProperties();
+            grantProperties.setGrantRemark("new context grant");
+            grantProperties.setAssignedOn(new Reference(contextId));
+            RoleRef roleRef = new RoleRef(roleObjectId);
+            grantProperties.setRole(roleRef);
+            grant.setGrantProperties(grantProperties);
+
+            grant = createGrant(userAccountId, grant);
+
+            // write out objid and last modification date
+            System.out.println("Grants set for User Account with objid='" + userAccountId + "' at '"
+                + grant.getLastModificationDate() + "'.");
+
+        }
+        catch (InternalClientException e) {
+            e.printStackTrace();
+        }
+        catch (TransportException e) {
+            e.printStackTrace();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (EscidocException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Grant createGrant(final String userAccountId, Grant grant) throws TransportException,
+        MalformedURLException, EscidocException, InternalClientException {
+
+        // prepare client object
+        Authentication auth =
+            new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN,
+                Constants.USER_PASSWORD_SYSADMIN);
+        UserAccountHandlerClient client = new UserAccountHandlerClient(auth.getServiceAddress());
+        client.setHandle(auth.getHandle());
+
+        return client.createGrant(userAccountId, grant);
+    }
+
+}
