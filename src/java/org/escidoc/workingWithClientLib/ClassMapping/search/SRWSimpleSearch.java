68e59aea2f5175fc2ac142bb41b906256794349b
St. Wagner <Steffen.Wagner@fiz-karlsruhe.de>
2011-05-06 13:25:31 +0000

    update dependency to right JiBX version

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/search/SRWSimpleSearch.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/search/SRWSimpleSearch.java
@@ -23,64 +23,61 @@ import de.escidoc.core.resources.sb.search.SearchRetrieveResponse;
  */
 public class SRWSimpleSearch {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-        String query = "PID=e*";
+		String query = "PID=e*";
 
-        if (args.length == 1) {
-            query = args[0];
-        }
+		if (args.length == 1) {
+			query = args[0];
+		}
 
-       // SRW Search
-        URL serviceAddress = null;
-        try {
-//            serviceAddress = new URL(Constants.DEFAULT_SERVICE_URL);
-          serviceAddress = new URL("http://localhost:8080/srw/search/escidoc_all");  
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+		// SRW Search
+		URL serviceAddress = null;
+		try {
+			serviceAddress = new URL(Constants.DEFAULT_SERVICE_URL
+					+ "/srw/search/escidoc_all");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 
-        SearchHandlerClient c = new SearchHandlerClient(serviceAddress);
+		SearchHandlerClient c = new SearchHandlerClient(serviceAddress);
 
-        SearchRetrieveResponse response = null;
-        try {
-            response = c.search(query, null);
-        }
-        catch (InternalClientException e) {
-            e.printStackTrace();
-        }
-        catch (TransportException e) {
-            e.printStackTrace();
-        }
-        catch (EscidocClientException e) {
-            e.printStackTrace();
-        }
+		SearchRetrieveResponse response = null;
+		try {
+			response = c.search(query, null);
+		} catch (InternalClientException e) {
+			e.printStackTrace();
+		} catch (TransportException e) {
+			e.printStackTrace();
+		} catch (EscidocClientException e) {
+			e.printStackTrace();
+		}
 
-        System.out.println("\n=========================\n");
-        System.out.println("Example for SRW Simple Search:\n");
-        System.out.println(" query = " + query);
-        System.out.println(" [RecordPacking: " + RecordPacking.XML.toString());
-        System.out.println();
-        System.out.println("]\n");
-        System.out.println("Results: ");
-        System.out.println(response.getNumberOfResultingRecords());
-        System.out.println("\n");
+		System.out.println("\n=========================\n");
+		System.out.println("Example for SRW Simple Search:\n");
+		System.out.println(" query = " + query);
+		System.out.println(" [RecordPacking: " + RecordPacking.XML.toString());
+		System.out.println();
+		System.out.println("]\n");
+		System.out.println("Results: ");
+		System.out.println(response.getNumberOfResultingRecords());
+		System.out.println("\n");
 
-        for (SearchResultRecord record : response.getRecords()) {
+		for (SearchResultRecord record : response.getRecords()) {
 
-            SearchResult data = record.getRecordData();
+			SearchResult data = record.getRecordData();
 
-            if (data.getContent() instanceof Resource) {
+			if (data.getContent() instanceof Resource) {
 
-                Resource res = (Resource) data.getContent();
+				Resource res = (Resource) data.getContent();
 
-                System.out.println(res.getResourceType().name() + ": ID[" + res.getObjid() + "], Href["
-                    + res.getXLinkHref() + "], Score[" + data.getScore() + "]\n");
-            }
-        }
-    }
+				System.out.println(res.getResourceType().name() + ": ID["
+						+ res.getObjid() + "], Href[" + res.getXLinkHref()
+						+ "], Score[" + data.getScore() + "]\n");
+			}
+		}
+	}
 }
