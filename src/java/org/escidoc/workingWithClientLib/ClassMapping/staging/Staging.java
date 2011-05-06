cfaebb0de5b13cdfc4b47f7940976af3215e2921
Joerg Hermann <Joerg.Hermann@fiz-karlsruhe.de>
2011-05-06 09:16:18 +0000

    example for staging

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/staging/Staging.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/staging/Staging.java
@@ -3,6 +3,7 @@ package org.escidoc.workingWithClientLib.ClassMapping.staging;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -35,7 +36,7 @@ public class Staging {
      */
     public static void main(String[] args) {
 
-        Authentication auth = null;;
+        Authentication auth = null;
         try {
             auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN,
                 Constants.USER_PASSWORD_SYSADMIN);
@@ -60,13 +61,26 @@ public class Staging {
 
         InputStream inputStream = null;
         try {
-            inputStream = load("/soap/om/item/0.6/item.xml");
+            inputStream = load("templates/generic/img/escidoc-logo.jpg");
+            int i = 0;
+            try {
+                while (inputStream.read() > -1) {
+                    i++;
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("eingelesen bytes: " + i);
+            
+            inputStream = load("templates/generic/img/escidoc-logo.jpg");
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         
         URL url = null;
+        
         try {
             url = sthc.upload(inputStream);
         }
@@ -79,10 +93,9 @@ public class Staging {
         catch (TransportException e) {
             e.printStackTrace();
         }
-
-        String stagingFile = "";
         
-        String s;
+        System.out.println("url: " + url.toString());
+  
         InputStream ins = null;
         try {
             ins = url.openStream();
@@ -91,48 +104,22 @@ public class Staging {
             e.printStackTrace();
         }
         
-        DataInputStream dis = new DataInputStream(new BufferedInputStream(ins));
         try {
-            while ((s = dis.readLine()) != null) {
-                stagingFile += s;
+            int i = 0;
+            while (ins.read() > -1) {
+                i++;
             }
+            
+           System.out.println("gelesen bytes: " + i);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
-        try {
-            inputStream = load("/soap/om/item/0.6/item.xml");
-        }
-        catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        }
-
-        String localFile = "";
-        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-        try {
-            String line;
-            try {
-                while ((line = in.readLine()) != null) {
-                    localFile += line;
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        finally {
-            try {
-                in.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
     
     /**
-     * Get FileInputStream from template.
+     * Get InputStream from template.
      * 
      * @param path
      *            Path to template file
@@ -143,11 +130,15 @@ public class Staging {
      */
     public static InputStream load(final String path)
         throws FileNotFoundException {
+        
+        try {
+            return new FileInputStream(path);
+        }
+        catch (java.io.FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        if (path == null)
-            return null;
-
-        return Template.class.getResourceAsStream(path);
+        return null;
     }
 
 }
