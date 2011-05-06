7546490be3c319d756b5b784c024c56da68dc821
Joerg Hermann <Joerg.Hermann@fiz-karlsruhe.de>
2011-05-06 10:47:01 +0000

    formatted

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/search/Explain.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/search/Explain.java
@@ -26,13 +26,13 @@ public class Explain {
      * @param args
      */
     public static void main(String[] args) {
-        
+
         ItemHandlerClientInterface c = new ItemHandlerClient();
 
         ExplainRequestType request = new ExplainRequestType();
-        
-        ExplainResponse response =  null;
-        
+
+        ExplainResponse response = null;
+
         try {
             response = c.retrieveItems(request);
         }
@@ -45,24 +45,22 @@ public class Explain {
         catch (TransportException e) {
             e.printStackTrace();
         }
-        
+
         Record<de.escidoc.core.resources.sb.explain.Explain> record = response.getRecord();
-        
         de.escidoc.core.resources.sb.explain.Explain data = record.getRecordData();
 
         List<Index> indexes = data.getIndexInfo().getIndexes();
-        
-            System.out.println("\n=========================\n");
-            System.out.println("Test Explain: ");
-            System.out.println("RecordPacking: " + RecordPacking.XML.toString());
-            System.out.println("\n=========================\n");
-            System.out.println("available search fields for Items:\n");
-            for (Index index : indexes){
-                System.out.println(index.getTitle());
-            }
-            
-            System.out.println("\n");
-            
+
+        System.out.println("\n=========================\n");
+        System.out.println("Example for Explain: ");
+        System.out.println("RecordPacking: " + RecordPacking.XML.toString());
+        System.out.println("\n=========================\n");
+        System.out.println("available search fields for Items:\n");
+        for (Index index : indexes) {
+            System.out.println(index.getTitle());
+        }
+        System.out.println("\n");
+
     }
-    
+
 }
