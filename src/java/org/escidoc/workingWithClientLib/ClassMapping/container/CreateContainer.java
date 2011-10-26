5cde1294a425accea22efcc1762e81bcc81744f9
St. Wagner <Steffen.Wagner@fiz-karlsruhe.de>
2011-10-26 15:12:07 +0000

    use depositor to create container

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/container/CreateContainer.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/container/CreateContainer.java
@@ -67,7 +67,7 @@ public class CreateContainer {
 			ParserConfigurationException, MalformedURLException {
 		
 		// prepare client object
-		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN, Constants.USER_PASSWORD_SYSADMIN);
+		Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_DEPOSITOR, Constants.USER_PASSWORD_DEPOSITOR);
 		ContainerHandlerClient chc = new ContainerHandlerClient(auth.getServiceAddress());
 		chc.setHandle(auth.getHandle());
 
