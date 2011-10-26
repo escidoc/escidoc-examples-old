132a56def1dac7a2144e115fdcee14e179d76949
St. Wagner <Steffen.Wagner@fiz-karlsruhe.de>
2011-10-26 15:37:09 +0000

    this should be an example for update with java objects and not REST xml handling - now it is

--- src/java/org/escidoc/workingWithClientLib/ClassMapping/item/UpdateItem.java
+++ src/java/org/escidoc/workingWithClientLib/ClassMapping/item/UpdateItem.java
@@ -1,77 +1,123 @@
 package org.escidoc.workingWithClientLib.ClassMapping.item;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.escidoc.Constants;
-import org.escidoc.simpleConnections.Util;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import de.escidoc.core.client.Authentication;
+import de.escidoc.core.client.ItemHandlerClient;
 import de.escidoc.core.client.exceptions.EscidocClientException;
-import de.escidoc.core.client.rest.RestItemHandlerClient;
+import de.escidoc.core.resources.common.MetadataRecord;
+import de.escidoc.core.resources.om.item.Item;
 
+/**
+ * Show how to update an Item in the infrastructure using Java objects.
+ * 
+ * @author SWA
+ * 
+ */
 public class UpdateItem {
 
-    public static void main(String[] args) {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-        String id = "escidoc:1";
-        String xmlFile = "templates/TUE/Item_update.xml";
+		String id = "escidoc:1";
 
-        try {
+		try {
+			Item item = retrieveItem(id);
+			System.out.println("Item with objid='" + item.getObjid() + "' at '"
+					+ item.getLastModificationDate() + "' in version no. "
+					+ item.getProperties().getVersion().getNumber()
+					+ " retrieved.");
 
-            String updatedResource =
-                updateItem(id, Util.getXmlFileAsString(xmlFile));
+			// add a meta data record to the Item
+			DocumentBuilderFactory factory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			Element element = doc.createElementNS(null, "an_empty_element");
 
-            String[] objidLmd = Util.obtainObjidAndLmd(updatedResource);
-            System.out.println("Item with objid='" + objidLmd[0] + "' at '"
-                + objidLmd[1] + "' updated.");
+			MetadataRecord mdRecord = new MetadataRecord(
+					"further_meta_data_record");
+			mdRecord.setContent(element);
 
-        }
-        catch (EscidocClientException e) {
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            // can not load xml, create it
-            try {
+			item.getMetadataRecords().add(mdRecord);
 
-                Util.storeXmlStringAsFile(retrieveItem(id), xmlFile);
+			item = updateItem(item);
+			System.out.println("Item with objid='" + item.getObjid() + "' at '"
+					+ item.getLastModificationDate()
+					+ "' updated to version no. "
+					+ item.getProperties().getVersion().getNumber() + ".");
 
-            }
-            catch (EscidocClientException ee) {
-                ee.printStackTrace();
-            }
-            catch (IOException ee) {
-                System.out.println("Can neither read nor write " + xmlFile
-                    + ".");
-            }
-        }
-    }
+		} catch (EscidocClientException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private static String updateItem(String itemId, String itemXml)
-        throws EscidocClientException, MalformedURLException {
-    	
-    	// prepare client object
-        Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN, Constants.USER_PASSWORD_SYSADMIN);
-        RestItemHandlerClient client = new RestItemHandlerClient(auth.getServiceAddress());
-        client.setHandle(auth.getHandle());
-        
-        String createdItem = client.update(itemId, itemXml);
+	/**
+	 * Update an Item in the infrastructure
+	 * 
+	 * @param item
+	 *            The Item to update
+	 * @return updated Item
+	 * 
+	 * @throws EscidocClientException
+	 * @throws MalformedURLException
+	 */
+	private static Item updateItem(final Item item)
+			throws EscidocClientException, MalformedURLException {
 
-        return createdItem;
-    }
+		// get authentication
+		Authentication auth = new Authentication(new URL(
+				Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_DEPOSITOR,
+				Constants.USER_PASSWORD_DEPOSITOR);
 
-    private static String retrieveItem(String itemId)
-        throws EscidocClientException, MalformedURLException {
-    	
-    	// prepare client object
-        Authentication auth = new Authentication(new URL(Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_SYSADMIN, Constants.USER_PASSWORD_SYSADMIN);
-        RestItemHandlerClient client = new RestItemHandlerClient(auth.getServiceAddress());
-        client.setHandle(auth.getHandle());
-        
-        String itemXml = client.retrieve(itemId);
+		// get service handler
+		ItemHandlerClient client = new ItemHandlerClient(
+				auth.getServiceAddress());
+		client.setHandle(auth.getHandle());
 
-        return itemXml;
-    }
+		return client.update(item);
+	}
+
+	/**
+	 * Retrieve an Item from the infrastructure.
+	 * 
+	 * @param itemId
+	 *            objid of the Item
+	 * @return Item resource
+	 * 
+	 * @throws EscidocClientException
+	 * @throws MalformedURLException
+	 */
+	private static Item retrieveItem(final String itemId)
+			throws EscidocClientException, MalformedURLException {
+
+		// get authentication
+		Authentication auth = new Authentication(new URL(
+				Constants.DEFAULT_SERVICE_URL), Constants.USER_NAME_DEPOSITOR,
+				Constants.USER_PASSWORD_DEPOSITOR);
+
+		// get service handler
+		ItemHandlerClient client = new ItemHandlerClient(
+				auth.getServiceAddress());
+		client.setHandle(auth.getHandle());
+
+		return client.retrieve(itemId);
+	}
 
 }
